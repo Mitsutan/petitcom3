1cf17d90536610c747ae32624f641ea8ca1ef977
Mitsutan <petitcom_hondou@icloud.com>
2023-04-11 23:59:56 +0900

    Create prjlist.php
    右端の作品リスト

---
+++ php/prjlist.php
@@ -0,0 +1,22 @@
+<?php
+    echo '<div class="col-4">';
+    echo '<div class="section">';
+    echo '<h1>新着</h1>';
+    echo '<ul>';
+    echo '<li>データがありません</li>';
+    echo '</ul>';
+    echo '</div>';
+    echo '<div class="section">';
+    echo '<h1>人気の作品</h1>';
+    echo '<ul>';
+    echo '<li>データがありません</li>';
+    echo '</ul>';
+    echo '</div>';
+    echo '<div class="section">';
+    echo '<h1>ランダム</h1>';
+    echo '<ul>';
+    echo '<li>データがありません</li>';
+    echo '</ul>';
+    echo '</div>';
+    echo '</div>';
+?>

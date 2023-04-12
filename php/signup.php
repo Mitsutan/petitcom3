950cab0e78daca5cef2164582b343024829363c9
Mitsutan <petitcom_hondou@icloud.com>
2023-04-13 00:26:16 +0900

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -11,6 +11,7 @@
             $db->submitUser($_POST['mail'], $_POST['name'], $_POST['pass'], $_POST['nnid']);
         } catch(LogicException $e) {
             echo $e->getMessage();
+            exit;
         } catch(PDOException $e) {
             echo '['.$e->getCode().']';               
             if($e->getCode() == '23000') {
@@ -19,8 +20,10 @@
             if($e->getCode() == '2002') {
                 echo '情報の登録に失敗しました。<br />しばらく時間をおいて再度お試しください。';
             }
+            exit;
         } catch(Exception $e) {
             echo $e->getMessage();
+            exit;
         }
 
             // 新規登録完了

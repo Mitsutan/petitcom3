5aec1e0b44709206f65d3812307b255a5d3a06d4
Mitsutan <petitcom_hondou@icloud.com>
2023-04-12 12:22:19 +0900

    Update writing.php
    関数化

--- php/writing.php
+++ php/writing.php
@@ -1,18 +1,15 @@
 <?php
+    // セッション
+    session_start();
+        
+    // データベースマネージャの読込
+    require_once "./DBManager.php";
+    $db = new DBManager();
+
     if (is_uploaded_file($_FILES['projectimg']['tmp_name'])) {
         $file='../img/projectimg/'.basename($_FILES['projectimg']['name']);
     }
 
-
-    // POSTされた文字列を取得
-    $str = $_POST['projectdesc'];
-
-    // 改行を削除して一行にする
-    $str = str_replace(array("\r\n", "\r", "\n"), '', $str);
-
-    $htmlString = strip_tags($str, '<a><p><br><b><i><u><ul><ol><li><h3>');
-    $regex = '/<(?!a\s)(\w+)\s*([^>]*?)(?<!\/)>/';
-    $result = preg_replace($regex, '<$1>', $htmlString);
-    echo $result;
-
+    $desc = $db->regexHtml($_POST['projectdesc']);
+    echo $desc;
 ?>

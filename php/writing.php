dcb4c0ca0409cdd6c8416899187bf539e4576f0a
Mitsutan <petitcom_hondou@icloud.com>
2023-04-12 11:21:28 +0900

    Update writing.php
    送信された文字列の正規化

--- php/writing.php
+++ php/writing.php
@@ -1,5 +1,18 @@
 <?php
-    if (is_uploaded_file($_FILES['file']['tmp_name'])) {
-        $file='../img/projectimg/'.basename($_FILES['file']['name']);
+    if (is_uploaded_file($_FILES['projectimg']['tmp_name'])) {
+        $file='../img/projectimg/'.basename($_FILES['projectimg']['name']);
     }
+
+
+    // POSTされた文字列を取得
+    $str = $_POST['projectdesc'];
+
+    // 改行を削除して一行にする
+    $str = str_replace(array("\r\n", "\r", "\n"), '', $str);
+
+    $htmlString = strip_tags($str, '<a><p><br><b><i><u><ul><ol><li><h3>');
+    $regex = '/<(?!a\s)(\w+)\s*([^>]*?)(?<!\/)>/';
+    $result = preg_replace($regex, '<$1>', $htmlString);
+    echo $result;
+
 ?>

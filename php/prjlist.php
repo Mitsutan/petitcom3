a863b0a24070d22286d1d9de7128b01549b7b28b
Mitsutan <petitcom_hondou@icloud.com>
2023-04-13 18:22:16 +0900

    Update prjlist.php
    新着順を取得反映

--- php/prjlist.php
+++ php/prjlist.php
@@ -3,7 +3,14 @@
     echo '<div class="section">';
     echo '<h1>新着</h1>';
     echo '<ul>';
-    echo '<li>データがありません</li>';
+    $dates = $db->getProjectsNew(5);
+    if (empty($dates)) {
+        echo '<li>データがありません</li>';
+    } else {
+        foreach ($dates as $key) {
+            echo '<li><a href="./project.html?id='.$key['project_id'].'">'.$key['project_name'].'</a></li>';
+        }
+    }
     echo '</ul>';
     echo '</div>';
     echo '<div class="section">';

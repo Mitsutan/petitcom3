ba1f240a915689d32c6be88e1f657fa93b982985
Mitsutan <petitcom_hondou@icloud.com>
2023-04-13 21:23:49 +0900

    Update prjlist.php
    ランダムに記事を選ぶ

--- php/prjlist.php
+++ php/prjlist.php
@@ -16,13 +16,20 @@
     echo '<div class="section">';
     echo '<h1>人気の作品</h1>';
     echo '<ul>';
-    echo '<li>データがありません</li>';
+    echo '<li>未実装(coming soon)</li>';
     echo '</ul>';
     echo '</div>';
     echo '<div class="section">';
     echo '<h1>ランダム</h1>';
     echo '<ul>';
-    echo '<li>データがありません</li>';
+    $dates = $db->getProjectsRand(5);
+    if (empty($dates)) {
+        echo '<li>データがありません</li>';
+    } else {
+        foreach ($dates as $key) {
+            echo '<li><a href="./project.html?id='.$key['project_id'].'">'.$key['project_name'].'</a></li>';
+        }
+    }
     echo '</ul>';
     echo '</div>';
     echo '</div>';

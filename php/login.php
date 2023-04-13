b29bb439f775653b4521c5f93b2c8370801613d4
Mitsutan <petitcom_hondou@icloud.com>
2023-04-13 12:48:23 +0900

    Update login.php

--- php/login.php
+++ php/login.php
@@ -11,11 +11,12 @@
             $result = $db->loginUser($_POST["mail"],$_POST["pass"]);
             // ユーザ情報をセッション変数に入れてトップページに飛ばす
             $_SESSION['login_id'] = $result['user_id'];
+            $_SESSION['login_auth'] = $result['user_auth'];
             // $_SESSION['login_nnid'] = $result['user_nnid'];
             echo "<script>location.replace('../index.html');</script>";
         } catch(PDOException $e) {
             if($e->getCode() == '2002') {
-                echo '情報の登録に失敗しました。<br />しばらく時間をおいて再度お試しください。';
+                echo '失敗しました。<br />しばらく時間をおいて再度お試しください。';
             }
         } catch(Exception $e) {
             // エラー表示

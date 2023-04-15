6653b1eb4690c7b950bebd958d2462b394a0d079
Mitsutan <petitcom_hondou@icloud.com>
2023-04-15 15:58:20 +0900

    Update project.php
    無駄なことしてたので削除

--- php/project.php
+++ php/project.php
@@ -1,9 +1,6 @@
 <?php
     // セッション
     session_start();
-    if (!isset($_SESSION['login_id'])) {
-        header("Location: ./login.html");
-    }
 
     // データベースマネージャの読込
     require_once "./DBManager.php";

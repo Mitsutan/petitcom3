4e3f39ca922bb208f54fc0739e37f5670c6415cd
Mitsutan <petitcom_hondou@icloud.com>
2023-04-15 22:01:00 +0900

    無駄な処理を削除
    echo無くていいって今まで知らなかった

--- php/nav.php
+++ php/nav.php
@@ -1,27 +1,27 @@
-<?php
-    echo '<nav id="navigation">';
-    echo '<div class="container-fluid m-0 bg-dark bg-gradient">';
-    echo '<div class="row">';
-    echo '<div class="col">';
-    echo '<a href="index.html"><img src="" alt="プチコン３号作品倉庫"></a>';
-    echo '</div>';
-    echo '<div class="col">';
-    echo '<ul>';
-    if (isset($_SESSION['login_id'])) {
-        echo '<li><a href="./php/logout.php">ログアウト</a></li>';
-        echo '<li><a href="./writing.html">新規記事投稿</a></li>';
-        if ($_SESSION['login_auth'] >= 1) {
-            echo '<li><a href="./mypage.html">プロフィール</a></li>';
-        }
-    } else {
-        echo '<li><a href="./login.html">ログイン</a></li>';
-    }
-    echo '<li><a href="./search.html">検索</a></li>';
-    echo '<li><a href="./contact.html">問い合わせ</a></li>';
-    echo '<li><a href="./readme.html">READ ME</a></li>';
-    echo '</ul>';
-    echo '</div>';
-    echo '</div>';
-    echo '</div>';
-    echo '</nav>';
-?>
+<nav id="navigation">
+    <div class="container-fluid m-0 bg-dark bg-gradient">
+        <div class="row">
+            <div class="col">
+                <a href="index.html"><img src="" alt="プチコン３号作品倉庫"></a>
+            </div>
+            <div class="col">
+                <ul>
+                    <?php
+                    if (isset($_SESSION['login_id'])) {
+                        echo '<li><a href="./php/logout.php">ログアウト</a></li>';
+                        echo '<li><a href="./writing.html">新規記事投稿</a></li>';
+                        if ($_SESSION['login_auth'] >= 1) {
+                            echo '<li><a href="./mypage.html">プロフィール</a></li>';
+                        }
+                    } else {
+                        echo '<li><a href="./login.html">ログイン</a></li>';
+                    }
+                    ?>
+                    <li><a href="./search.html">検索</a></li>
+                    <li><a href="./contact.html">問い合わせ</a></li>
+                    <li><a href="./readme.html">READ ME</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
--- php/prjlist.php
+++ php/prjlist.php
@@ -1,36 +1,36 @@
-<?php
-    echo '<div class="col-4">';
-    echo '<div class="section">';
-    echo '<h1>新着</h1>';
-    echo '<ul>';
-    $dates = $db->getProjectsNew(5);
-    if (empty($dates)) {
-        echo '<li>データがありません</li>';
-    } else {
-        foreach ($dates as $key) {
-            echo '<li><a href="./project.html?id='.$key['project_id'].'">'.$key['project_name'].'</a></li>';
-        }
-    }
-    echo '</ul>';
-    echo '</div>';
-    echo '<div class="section">';
-    echo '<h1>人気の作品</h1>';
-    echo '<ul>';
-    echo '<li>未実装(coming soon)</li>';
-    echo '</ul>';
-    echo '</div>';
-    echo '<div class="section">';
-    echo '<h1>ランダム</h1>';
-    echo '<ul>';
-    $dates = $db->getProjectsRand(5);
-    if (empty($dates)) {
-        echo '<li>データがありません</li>';
-    } else {
-        foreach ($dates as $key) {
-            echo '<li><a href="./project.html?id='.$key['project_id'].'">'.$key['project_name'].'</a></li>';
-        }
-    }
-    echo '</ul>';
-    echo '</div>';
-    echo '</div>';
-?>
+<div class="col-4">
+    <div class="section">
+        <h1>新着</h1>
+        <ul>
+            <?php
+            $dates = $db->getProjectsNew(5);
+            if (empty($dates)) {
+                echo '<li>データがありません</li>';
+            } else {
+                foreach ($dates as $key) {
+                    echo '<li><a href="./project.html?id=' . $key['project_id'] . '">' . $key['project_name'] . '</a></li>';
+                }
+            }
+            echo '</ul>';
+            echo '</div>';
+            echo '<div class="section">';
+            echo '<h1>人気の作品</h1>';
+            echo '<ul>';
+            echo '<li>未実装(coming soon)</li>';
+            echo '</ul>';
+            echo '</div>';
+            echo '<div class="section">';
+            echo '<h1>ランダム</h1>';
+            echo '<ul>';
+            $dates = $db->getProjectsRand(5);
+            if (empty($dates)) {
+                echo '<li>データがありません</li>';
+            } else {
+                foreach ($dates as $key) {
+                    echo '<li><a href="./project.html?id=' . $key['project_id'] . '">' . $key['project_name'] . '</a></li>';
+                }
+            }
+            ?>
+        </ul>
+    </div>
+</div>

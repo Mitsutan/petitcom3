9f1593a819df63083a8f06308aa594a176a283fe
Mitsutan <petitcom_hondou@icloud.com>
2023-05-04 00:15:06 +0900

    Update nav.php

--- php/nav.php
+++ php/nav.php
@@ -2,7 +2,7 @@
     <div class="container-fluid m-0 bg-dark bg-gradient">
         <div class="row">
             <div class="col">
-                <a href="index.html"><img src="" alt="プチコン３号作品倉庫"></a>
+                <a href="index.html"><img src="" alt="プチコン３号作品倉庫（ロゴ画像未定）"></a>
             </div>
             <div class="col">
                 <ul>

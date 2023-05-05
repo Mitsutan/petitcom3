5788bcfe72757a1d1f46a2231c4a52f72e69d837
Mitsutan <petitcom_hondou@icloud.com>
2023-05-05 10:47:29 +0900

    ロゴ画像位置修正
    %指定だとうまく動かない、何故

--- php/nav.php
+++ php/nav.php
@@ -1,8 +1,8 @@
 <nav id="navigation">
     <div class="container-fluid m-0 bg-dark bg-gradient">
         <div class="row">
-            <div class="col">
-                <a href="index.php"><img src="./img/logo.png" alt="プチコン３号作品倉庫（ロゴ画像未定）" id="logo"></a>
+            <div class="col my-auto">
+                <a href="index.php"><img src="./img/logo.png" alt="プチコン３号作品倉庫" id="logo"></a>
             </div>
             <div class="col">
                 <ul>

db4f6a2b1c4cc687619416485c49f907f3d75dad
Mitsutan <petitcom_hondou@icloud.com>
2023-05-09 11:54:53 +0900

    Create 503.php

---
+++ 503.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>503 Error</title>
+</head>
+<body>
+    <p>
+        試験運用は終了しました。<br />
+        多少の修正を加えた後、本公開されます。しばらくお待ちください。
+        時期は未定です。
+    </p>
+    <p>
+        問い合わせ<br />
+        twitter:@ptcm_kondou<br />
+        petitverse:http://petitverse.hosiken.jp/community/petitcom/topic/?read=1828
+    </p>
+</body>
+</html>

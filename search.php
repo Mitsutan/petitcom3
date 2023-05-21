cda2e1d11900130579b94939348b9ef9b51bf7ac
Mitsutan <petitcom_hondou@icloud.com>
2023-05-21 10:42:19 +0900

    Update search.php

--- search.php
+++ search.php
@@ -86,6 +86,7 @@ $db = new DBManager();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
+    <!-- <script>console.log("<?php //krsort($res); print_r($res) ?>");</script> -->
 </body>
 
 </html>

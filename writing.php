7121da94d26cf533d05ed90ef213e0d1af573990
Mitsutan <petitcom_hondou@icloud.com>
2023-06-19 14:49:04 +0900

    Update writing.php
    画像アップロード非対応厳格化

--- writing.php
+++ writing.php
@@ -19,8 +19,7 @@ $db->OutPutlog();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.4/dist/trix.css">
@@ -53,24 +52,8 @@ $db->OutPutlog();
                         </div>
                         <div>
                             <p>概要</p>
-                            <!-- <textarea name="projectdesc" id="" cols="35" rows="10"
-                                placeholder="<h3>操作方法</h3>"></textarea> -->
                             <input id="desc" type="hidden" name="projectdesc">
                             <trix-editor input="desc" class="trix-content"></trix-editor>
-                            <!-- <div>
-                                次のhtmlタグが使用できます：
-                                a(リンク)
-                                p(段落)
-                                br(改行)
-                                i(斜体)
-                                b(太字)
-                                u(下線)
-                                ul(箇条書きリスト、liと併用)
-                                ol(番号付きリスト、liと併用)
-                                li(リスト項目)
-                                h3(見出し)<br />
-                                style属性は使用できません。（無視されます）
-                            </div> -->
                         </div>
                         <div>
                             <p>タグ</p>
@@ -88,9 +71,13 @@ $db->OutPutlog();
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+        addEventListener('trix-file-accept', function(event) {
+            event.preventDefault();
+            alert('画像はアップロードできません。');
+        });
+    </script>
 </body>
 
 </html>

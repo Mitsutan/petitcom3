7cc40eef8b05f419c6f5bd46d7c292d4e609e939
Mitsutan <petitcom_hondou@icloud.com>
2023-06-20 09:34:15 +0900

    favicon追加
    index以外のページにつけてなかったので

--- project.php
+++ project.php
@@ -51,6 +51,8 @@ $rtn = $db->cntView($_GET['id']);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="icon" href="./img/favicon.ico">
+
     <!-- OGP -->
     <meta property="og:type" content="article" />
     <meta property="og:title" content="<?php echo $result['project_name'] ?>" />
